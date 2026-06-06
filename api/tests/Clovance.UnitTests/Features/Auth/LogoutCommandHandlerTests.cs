eb0e7d9acf9343e5102c83ac3cff88eed5f2ca6f
evaristocuesta <evaristocg@gmail.com>
2026-06-06 09:12:31 +0200

    Fix logout test

--- api/tests/Clovance.UnitTests/Features/Auth/LogoutCommandHandlerTests.cs
+++ api/tests/Clovance.UnitTests/Features/Auth/LogoutCommandHandlerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Clovance.ApiService.Domain.RefreshTokens;
 using Clovance.ApiService.Features.Auth.Logout;
+using Clovance.ApiService.Infrastructure.Authentication;
 using Clovance.ApiService.Infrastructure.Database;
 using Microsoft.AspNetCore.Http;
 using NSubstitute;
@@ -12,6 +13,7 @@ public class LogoutCommandHandlerTests : IAsyncLifetime
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly HttpContext _httpContext;
     private readonly LogoutCommandHandler _handler;
+    private readonly IJwtTokenService _jwtTokenService;
 
     public LogoutCommandHandlerTests()
     {
@@ -20,8 +22,9 @@ public class LogoutCommandHandlerTests : IAsyncLifetime
         _httpContext = new DefaultHttpContext();
         _httpContextAccessor = Substitute.For<IHttpContextAccessor>();
         _httpContextAccessor.HttpContext.Returns(_httpContext);
+        _jwtTokenService = Substitute.For<IJwtTokenService>();
 
-        _handler = new LogoutCommandHandler(_httpContextAccessor, _dbContext);
+        _handler = new LogoutCommandHandler(_httpContextAccessor, _dbContext, _jwtTokenService);
     }
 
     public ValueTask InitializeAsync()
@@ -47,12 +50,14 @@ public class LogoutCommandHandlerTests : IAsyncLifetime
         }, TestContext.Current.CancellationToken);
 
         await _dbContext.RefreshTokens.AddAsync(
-            RefreshToken.Create(userId.ToString(), "token", DateTimeOffset.UtcNow.AddDays(7)), 
+            RefreshToken.Create(userId.ToString(), "hashedToken", DateTimeOffset.UtcNow.AddDays(7)), 
             TestContext.Current.CancellationToken);
 
         await _dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
 
         _httpContext.Request.Headers.Cookie = "refreshToken=token";
+
+        _jwtTokenService.HashToken(Arg.Any<string>()).Returns("hashedToken");   
 
         var command = new LogoutCommand();
 
