08f023f96f8427e2fd08c80796fa69c730e60b7c
evaristocuesta <evaristocg@gmail.com>
2026-06-09 01:09:08 +0200

    Add layouts, login, auth guard and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from '@layouts/auth-layout/auth-layout';
+import { MainLayout } from '@layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth', 
+        component: AuthLayout, 
+        children: [
+            { path: 'login', loadComponent: () => import('@features/auth/login/login').then(m => m.Login) },
+            { path: '', redirectTo: 'login', pathMatch: 'full' }
+        ]
+    }, 
+    {
+        path: '', 
+        component: MainLayout, 
+        children: [
+            { path: 'aaa', loadComponent: () => import('@features/auth/login/login').then(m => m.Login) }, 
+            { path: '', redirectTo: 'aaa', pathMatch: 'full'}
+        ]
+        
+    }
+];
