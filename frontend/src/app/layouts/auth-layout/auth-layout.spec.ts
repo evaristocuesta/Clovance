1303d6b35fc7b0679681ff9983828bbf5dabd9bd
evaristocuesta <evaristocg@gmail.com>
2026-06-12 01:32:48 +0200

    Fix test

--- frontend/src/app/layouts/auth-layout/auth-layout.spec.ts
+++ frontend/src/app/layouts/auth-layout/auth-layout.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Translation, TranslocoLoader, provideTransloco } from '@jsverse/transloco';
+import { Observable, of } from 'rxjs';
 
 import { AuthLayout } from './auth-layout';
+
+class TranslocoTestingLoader implements TranslocoLoader {
+  getTranslation(): Observable<Translation> {
+    return of({});
+  }
+}
 
 describe('AuthLayout', () => {
   let component: AuthLayout;
@@ -9,6 +17,17 @@ describe('AuthLayout', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AuthLayout],
+      providers: [
+        provideTransloco({
+          config: {
+            availableLangs: ['en', 'es'],
+            defaultLang: 'en',
+            reRenderOnLangChange: true,
+            prodMode: true,
+          },
+          loader: TranslocoTestingLoader,
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthLayout);
