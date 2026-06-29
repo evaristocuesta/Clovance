8ab6334bb39497beb26f47b39b9f93be8e6f2556
evaristocuesta <evaristocg@gmail.com>
2026-06-29 19:36:45 +0200

    Add change password feature

---
+++ api/src/Clovance.ApiService/Features/Auth/ChangePassword/ChangePasswordCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace Clovance.ApiService.Features.Auth.ChangePassword;
+
+public sealed record ChangePasswordCommand(string CurrentPassword, string NewPassword);
---
+++ api/src/Clovance.ApiService/Features/Auth/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Clovance.ApiService.Features.Shared;
+using Clovance.ApiService.Infrastructure.Database;
+using Microsoft.AspNetCore.Identity;
+
+namespace Clovance.ApiService.Features.Auth.ChangePassword;
+
+public class ChangePasswordCommandHandler : IHandler<ChangePasswordCommand, Result>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public ChangePasswordCommandHandler(
+        UserManager<ApplicationUser> userManager,
+        IHttpContextAccessor httpContextAccessor)
+    {
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<Result> HandleAsync(ChangePasswordCommand command, CancellationToken cancellationToken)
+    {
+        var httpContext = _httpContextAccessor.HttpContext
+            ?? throw new InvalidOperationException("HttpContext is not available.");
+
+        var user = await _userManager.GetUserAsync(httpContext.User);
+
+        if (user is null)
+        {
+            return Result<Result>.Failure(AppErrors.Auth.UserNotFound());
+        }
+
+        var changePasswordResult = await _userManager.ChangePasswordAsync(user, command.CurrentPassword, command.NewPassword);
+
+        if (changePasswordResult.Succeeded)
+        {
+            return Result.Success();
+        }
+        else
+        {
+            var errors = string.Join(", ", changePasswordResult.Errors.Select(e => e.Description));
+            return Result<Result>.Failure(AppErrors.Auth.PasswordChangeFailed(errors));
+        }
+    }
+}
