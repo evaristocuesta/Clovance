8de70edc7263ed9af72b43abd6d6f3ea10b42907
evaristocuesta <evaristocg@gmail.com>
2026-06-29 19:36:45 +0200

    Add change password feature

---
+++ api/src/Clovance.ApiService/Features/Auth/ChangePassword/ChangePasswordEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Clovance.ApiService.Features.Shared;
+
+namespace Clovance.ApiService.Features.Auth.ChangePassword;
+
+public class ChangePasswordEndpoint : IApiEndPoint
+{
+    public void MapApiEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapPut("users/password", async (
+            ChangePasswordCommand command, 
+            IHandler<ChangePasswordCommand, Result> handler,
+            HttpContext httpContext,
+            CancellationToken cancellationToken) =>
+        {
+            var result = await handler.HandleAsync(command, cancellationToken);
+            
+            if (result.IsFailure)
+            {
+                return result.ToProblemResult(httpContext);
+            }
+
+            return Results.Ok();
+        })
+        .Produces(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .RequireAuthorization()
+        .WithName("ChangePassword")
+        .WithSummary("Change Password")
+        .WithDescription("Changes the password of an existing user.");
+    }
+}
---
+++ api/src/Clovance.ApiService/Features/Auth/ChangePassword/ChangePasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿using Clovance.ApiService.Infrastructure.Validation;
+using Clovance.ApiService.Shared;
+using FluentValidation;
+
+namespace Clovance.ApiService.Features.Auth.ChangePassword;
+
+public class ChangePasswordValidator : AbstractValidator<ChangePasswordCommand>
+{
+    public ChangePasswordValidator()
+    {
+        RuleFor(x => x.CurrentPassword)
+            .NotEmpty()
+            .WithErrorCode(ErrorCodes.Auth.CurrentPasswordRequired);
+        RuleFor(x => x.NewPassword)
+            .NotEmpty()
+            .WithErrorCode(ErrorCodes.Auth.NewPasswordRequired)
+            .ApplyPasswordPolicy();
+    }
+}
