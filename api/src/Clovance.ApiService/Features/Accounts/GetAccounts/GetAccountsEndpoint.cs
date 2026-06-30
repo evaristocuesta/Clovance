5f5e61c7eb082be31b83b0e6131b105f1cd2bd18
evaristocuesta <evaristocg@gmail.com>
2026-06-30 17:59:53 +0200

    Add GetAccounts endpoint #32

---
+++ api/src/Clovance.ApiService/Features/Accounts/GetAccounts/GetAccountsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using Clovance.ApiService.Features.Shared;
+
+namespace Clovance.ApiService.Features.Accounts.GetAccounts;
+
+public class GetAccountsEndpoint : IApiEndPoint
+{
+    public void MapApiEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/accounts", async (GetAccountsQueryHandler handler, CancellationToken cancellationToken) =>
+        {
+            var result = await handler.HandleAsync(new GetAccountsQuery(), cancellationToken);
+            return Results.Ok(result.Accounts);
+        })
+        .Produces<List<AccountDto>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status401Unauthorized)
+        .RequireAuthorization()
+        .WithName("GetAccounts")
+        .WithSummary("Get Accounts")
+        .WithDescription("Retrieve a list of all accounts.");
+    }
+}
---
+++ api/src/Clovance.ApiService/Features/Accounts/GetAccounts/GetAccountsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Clovance.ApiService.Features.Shared;
+using Clovance.ApiService.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clovance.ApiService.Features.Accounts.GetAccounts;
+
+public class GetAccountsQueryHandler : IHandler<GetAccountsQuery, GetAccountsQueryResult>
+{
+    private readonly ClovanceDbContext _dbContext;
+
+    public GetAccountsQueryHandler(ClovanceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<GetAccountsQueryResult> HandleAsync(GetAccountsQuery command, CancellationToken cancellationToken)
+    {
+        var accounts = await _dbContext.Accounts
+            .Select(a => a.ToDto())
+            .ToListAsync(cancellationToken);
+
+        return new GetAccountsQueryResult(accounts);
+    }
+}
