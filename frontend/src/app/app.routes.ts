3c3957182c503090bcfb3a1f72b78be368a960df
evaristocuesta <evaristocg@gmail.com>
2026-07-01 18:50:09 +0200

    Move summary, accounts and transactions components #33

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -22,9 +22,9 @@ export const routes: Routes = [
         component: MainLayout, 
         canActivate: [authGuard],
         children: [
-            { path: 'accounts', loadComponent: () => import('@features/accounts/accounts').then(m => m.Accounts) }, 
-            { path: 'transactions', loadComponent: () => import('@features/transactions/transactions').then(m => m.Transactions) }, 
-            { path: 'summary', loadComponent: () => import('@features/summary/summary').then(m => m.Summary) }, 
+            { path: 'accounts', loadComponent: () => import('@features/accounts/accounts/accounts').then(m => m.Accounts) }, 
+            { path: 'transactions', loadComponent: () => import('@features/transactions/transactions/transactions').then(m => m.Transactions) }, 
+            { path: 'summary', loadComponent: () => import('@features/summary/summary/summary').then(m => m.Summary) }, 
             { path: 'users', loadComponent: () => import('@features/auth/users/users').then(m => m.Users) }, 
             { path: 'account-settings', loadComponent: () => import('@features/auth/account-settings/account-settings').then(m => m.AccountSettings) },
             { path: '', redirectTo: 'accounts', pathMatch: 'full'}
