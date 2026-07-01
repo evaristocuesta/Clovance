2d11ff22ab895ac0f0c332a3eec6422e45e1bddd
evaristocuesta <evaristocg@gmail.com>
2026-07-01 18:07:16 +0200

    Add get account by id endpoint #32

---
+++ api/src/Clovance.ApiService/Features/Accounts/GetAccountById/GetAccountByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace Clovance.ApiService.Features.Accounts.GetAccountById;
+
+public sealed record GetAccountByIdQuery(Guid Id);
+
+public sealed record GetAccountByIdResult(AccountDto? Account);
