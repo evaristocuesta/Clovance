7d415b405af5dba223c0397d646783a95de6640c
evaristocuesta <evaristocg@gmail.com>
2026-07-01 17:50:37 +0200

    Add create account #32

---
+++ api/src/Clovance.ApiService/Features/Accounts/CreateAccount/CreateAccountCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Clovance.ApiService.Features.Accounts.CreateAccount;
+
+public sealed record CreateAccountCommand(string Name, string Currency);
+
+public sealed record CreateAccountResult(AccountDto Account);
---
+++ api/src/Clovance.ApiService/Features/Accounts/CreateAccount/CreateAccountValidator.cs
@@ -0,0 +1,23 @@
+﻿using Clovance.ApiService.Domain.Accounts;
+using Clovance.ApiService.Shared;
+using FluentValidation;
+
+namespace Clovance.ApiService.Features.Accounts.CreateAccount;
+
+public class CreateAccountValidator : AbstractValidator<CreateAccountCommand>
+{
+    public CreateAccountValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithErrorCode(ErrorCodes.Accounts.AccountNameRequired)
+            .MaximumLength(200)
+            .WithErrorCode(ErrorCodes.Accounts.AccountNameMaxLength);
+
+        RuleFor(x => x.Currency)
+            .NotEmpty()
+            .WithErrorCode(ErrorCodes.Accounts.AccountCurrencyInvalid)
+            .Must(CurrencyValidator.IsValid)
+            .WithErrorCode(ErrorCodes.Accounts.AccountCurrencyInvalid);
+    }
+}
