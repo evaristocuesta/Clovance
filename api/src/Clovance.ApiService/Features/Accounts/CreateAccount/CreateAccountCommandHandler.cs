abcf7dc49bf357a7abb45eae9fe3901be6dbaff1
evaristocuesta <evaristocg@gmail.com>
2026-07-01 17:50:37 +0200

    Add create account #32

---
+++ api/src/Clovance.ApiService/Features/Accounts/CreateAccount/CreateAccountCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Clovance.ApiService.Domain.Accounts;
+using Clovance.ApiService.Features.Shared;
+using Clovance.ApiService.Infrastructure.Database;
+
+namespace Clovance.ApiService.Features.Accounts.CreateAccount;
+
+public class CreateAccountCommandHandler : IHandler<CreateAccountCommand, Result<CreateAccountResult>>
+{
+    private readonly ClovanceDbContext _context;
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public CreateAccountCommandHandler(ClovanceDbContext context, IHttpContextAccessor contextAccessor)
+    {
+        _context = context;
+        _contextAccessor = contextAccessor;
+    }
+
+    public async Task<Result<CreateAccountResult>> HandleAsync(CreateAccountCommand command, CancellationToken cancellationToken)
+    {
+        var email = _contextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.Email)?.Value ?? "system";
+
+        var account = await _context.Accounts.AddAsync(
+            Account.Create(command.Name, command.Currency, email), 
+            cancellationToken);
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return Result<CreateAccountResult>.Success(new CreateAccountResult(account.Entity.ToDto()));
+    }
+}
---
+++ api/src/Clovance.ApiService/Features/Accounts/CreateAccount/CreateAccountEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Clovance.ApiService.Features.Shared;
+
+namespace Clovance.ApiService.Features.Accounts.CreateAccount;
+
+public class CreateAccountEndpoint : IApiEndPoint
+{
+    public void MapApiEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/", async (
+            CreateAccountCommand command, 
+            IHandler<CreateAccountCommand, Result<CreateAccountResult>> handler,
+            HttpContext httpContext,
+            CancellationToken cancellationToken) =>
+        {
+            var result = await handler.HandleAsync(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return result.ToProblemResult(httpContext);
+            }
+
+            return Results.Ok(result.Value.Account);
+        })
+        .Produces<AccountDto>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status401Unauthorized)
+        .RequireAuthorization()
+        .WithName("CreateAccount")
+        .WithSummary("Create Account")
+        .WithDescription("Creates a new account with the specified details.");
+    }
+}
