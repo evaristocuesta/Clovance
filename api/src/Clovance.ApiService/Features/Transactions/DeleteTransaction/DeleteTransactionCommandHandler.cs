b3c28a088a3ef5340b8980de150960f8984f1d72
evaristocuesta <evaristocg@gmail.com>
2026-08-01 11:04:50 +0200

    Delete transactions for transactions with related #76

--- api/src/Clovance.ApiService/Features/Transactions/DeleteTransaction/DeleteTransactionCommandHandler.cs
+++ api/src/Clovance.ApiService/Features/Transactions/DeleteTransaction/DeleteTransactionCommandHandler.cs
@@ -24,9 +24,47 @@ public class DeleteTransactionCommandHandler : IHandler<DeleteTransactionCommand
             return Result.Failure(AppErrors.Transactions.TransactionNotFound());
         }
 
-        _context.Transactions.Remove(transaction);
-        await _context.SaveChangesAsync();
-        
+        if (transaction.RelatedTransactionId.HasValue)
+        {
+            await DeleteTransactionWithRelated(transaction, cancellationToken);
+            return Result.Success();
+        }
+
+        await DeleteTransaction(transaction, cancellationToken);
         return Result.Success();
+    }
+
+    private async Task DeleteTransaction(Transaction transaction, CancellationToken cancellationToken)
+    {
+        _context.Transactions.Remove(transaction);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    private async Task DeleteTransactionWithRelated(Transaction transaction, CancellationToken cancellationToken)
+    {
+        var relatedTransaction = await _context
+            .Transactions
+            .FindAsync(transaction.RelatedTransactionId!.Value, cancellationToken);
+
+        await using var transactionScope = await _context.Database.BeginTransactionAsync(cancellationToken);
+
+        transaction.ChangeRelatedTransactionId(null);
+
+        if (relatedTransaction is not null)
+        {
+            relatedTransaction.ChangeRelatedTransactionId(null);
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        _context.Transactions.Remove(transaction);
+
+        if (relatedTransaction is not null)
+        {
+            _context.Transactions.Remove(relatedTransaction);
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+        await transactionScope.CommitAsync(cancellationToken);
     }
 }
