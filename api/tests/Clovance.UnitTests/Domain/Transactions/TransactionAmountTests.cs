77ba02dec079bb9dc53f337af16641d136103875
evaristocuesta <evaristocg@gmail.com>
2026-08-04 18:32:55 +0200

    Remove TransactionAmount test #76

--- api/tests/Clovance.UnitTests/Domain/Transactions/TransactionAmountTests.cs
+++ api/tests/Clovance.UnitTests/Domain/Transactions/TransactionAmountTests.cs
@@ -5,14 +5,6 @@ namespace Clovance.UnitTests.Domain.Transactions;
 public class TransactionAmountTests
 {
     [Fact]
-    public void Create_WithZero_ThrowsArgumentException()
-    {
-        var action = () => TransactionAmount.Create(0m);
-
-        Assert.Throws<ArgumentException>(action);
-    }
-
-    [Fact]
     public void Create_RoundsToTwoDecimals()
     {
         var amount = TransactionAmount.Create(12.345m);
